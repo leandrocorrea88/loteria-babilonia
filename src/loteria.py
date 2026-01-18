266e9c74092aa3e849b78f95cda0c3d037654e90
Leandro Correa <leandrocorrea.ep@gmail.com>
2026-01-18 17:54:57 -0300

    feat: mensagem com o sentido do erro de palpite (maior/menor)

--- src/loteria.py
+++ src/loteria.py
@@ -11,8 +11,10 @@ numero_da_sorte = 7
 # Input para o usuário digitar o número
 numero_usuario = int(input("Digite um número entre 1 e 15, por favor"))
 
-# Teste do resultado com o print
+# Teste do resultado com o print, indicando a direção do erro
 if numero_usuario == numero_da_sorte:
     print("Parabéns, você acertou o resultado")
+elif numero_usuario > numero_da_sorte :
+    print("Que pena, você errou. Tente um número menor na próxima vez")
 else:
-    print("Que pena, você errou. Tente novamente")
+    print("Que pena, você errou. Tente um número maior na próxima vez")
