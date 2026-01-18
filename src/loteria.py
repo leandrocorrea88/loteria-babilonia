db43214f3e328e63106325b7495ceb1d68162198
Leandro Correa <leandrocorrea.ep@gmail.com>
2026-01-18 19:21:34 -0300

    feat: N tentativas e Controle de Créditos
    Implementação de numero de créditos variáveis pelo usuário, garantia que valores não validados não consumam crédito e ampliação de mensageria de apoio (créditos restantes, sem créditos e Game Over)

--- src/loteria.py
+++ src/loteria.py
@@ -8,22 +8,44 @@ usuario a chance de acertar, com uma mensagem ao final indicando se acertou ou e
 # Numero da sorte que o usuário tentará acertar (hardcoded)
 numero_da_sorte = 7
 
-# Limitar as chances do usuário a, no maximo 3   
-for tentativa in range(3):
-
+# Input 1 : Numero de tentativas variavel, com validação
+while True:
     try:
-        
-        # Input para o usuário digitar o número
-        numero_usuario = int(input(f"Tentativa {tentativa + 1 }: Digite um número entre 1 e 15, por favor"))
-
-        # Teste do resultado com o print, indicando a direção do erro
-        if numero_usuario == numero_da_sorte:
-            print("Parabéns, você acertou o resultado")
-        elif numero_usuario > numero_da_sorte :
-            print("Que pena, você errou. Tente um número menor na próxima vez")
-        else:
-            print("Que pena, você errou. Tente um número maior na próxima vez") 
-
+        creditos = int(input("Digite o numero de créditos"))
+        # Ao passar na validação, sair do while e continuar o fluxo
+        break
     except ValueError as err:
-        print("Insira um valor numérico inteiro para fazer o teste")
-# %%
+        print("Por favor, insira um valor válido para a quantidade de créditos")
+
+creditos_restantes = creditos
+
+# Limitar as chances do usuário a, no maximo 3   
+for tentativa in range(creditos):
+
+    # Input para o usuário digitar o número, gastando crédito somente com valores válidos
+    while True:
+        try:
+            numero_usuario = int(input(f"Tentativa {tentativa+1}: Digite um número entre 1 e 15, por favor"))
+            break
+        except ValueError as err:
+            print("Por favor, insira um valor válido entre 1 e 15.\nSeu crédito atual não foi consumido")
+
+    # Consumir um credito
+    creditos_restantes -= 1
+
+    # Teste do resultado com o print, indicando a direção do erro e detalhando o acerto
+    if numero_usuario == numero_da_sorte:
+        print(f"Parabéns, você acertou o resultado {numero_usuario} na tentativa #{tentativa+1}!")
+        break
+    
+    # Testar se ainda existem créditos para uso
+    elif creditos_restantes > 0:
+        if numero_usuario > numero_da_sorte :
+            print("Que pena, você errou. Tente um número menor na próxima vez...")
+            print(f"Crédito(s) restante(s) : {creditos_restantes}")
+        else:
+            print("Que pena, você errou. Tente um número maior na próxima vez...") 
+            print(f"Crédito(s) restante(s) : {creditos_restantes}")
+    else:
+        print(f"GAME OVER: Você não tem mais tentativas. O número da sorte era {numero_da_sorte}")
+        print("Para começar outro jogo compre mais fichas")
