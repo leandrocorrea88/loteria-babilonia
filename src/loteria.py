7a06be27aca279e4c2c65b5e6e03419da6ba80e5
Leandro Correa <leandrocorrea.ep@gmail.com>
2026-01-18 18:20:14 -0300

    feat: incluir loop fixo de 3 chances, com validação de formato de entrada

--- src/loteria.py
+++ src/loteria.py
@@ -8,13 +8,13 @@ usuario a chance de acertar, com uma mensagem ao final indicando se acertou ou e
 # Numero da sorte que o usuário tentará acertar (hardcoded)
 numero_da_sorte = 7
 
-# Inserir loop para validar as entradas do usuário
-while True:
-    
+# Limitar as chances do usuário a, no maximo 3   
+for tentativa in range(3):
+
     try:
         
         # Input para o usuário digitar o número
-        numero_usuario = int(input("Digite um número entre 1 e 15, por favor"))
+        numero_usuario = int(input(f"Tentativa {tentativa + 1 }: Digite um número entre 1 e 15, por favor"))
 
         # Teste do resultado com o print, indicando a direção do erro
         if numero_usuario == numero_da_sorte:
@@ -23,11 +23,7 @@ while True:
             print("Que pena, você errou. Tente um número menor na próxima vez")
         else:
             print("Que pena, você errou. Tente um número maior na próxima vez") 
-    
-        # Sai do programa após o teste realizado
-        break
 
     except ValueError as err:
         print("Insira um valor numérico inteiro para fazer o teste")
-
 # %%
