d4c7f99548b44da6f13279f59cf7a5ada65c8071
Leandro Correa <leandrocorrea.ep@gmail.com>
2026-01-18 18:11:15 -0300

    feat: incluir validação de formato de entrada numero (loop while)

--- src/loteria.py
+++ src/loteria.py
@@ -1,6 +1,6 @@
 '''
 Programa para gerar uma interação divertida com o computador, criando um numero da sorte e dando ao
-usuario a chance de acertar, com uma mensagem ao final indicando se acetou ou errou
+usuario a chance de acertar, com uma mensagem ao final indicando se acertou ou errou
 '''
 
 # %%
@@ -8,13 +8,26 @@ usuario a chance de acertar, com uma mensagem ao final indicando se acetou ou er
 # Numero da sorte que o usuário tentará acertar (hardcoded)
 numero_da_sorte = 7
 
-# Input para o usuário digitar o número
-numero_usuario = int(input("Digite um número entre 1 e 15, por favor"))
+# Inserir loop para validar as entradas do usuário
+while True:
+    
+    try:
+        
+        # Input para o usuário digitar o número
+        numero_usuario = int(input("Digite um número entre 1 e 15, por favor"))
 
-# Teste do resultado com o print, indicando a direção do erro
-if numero_usuario == numero_da_sorte:
-    print("Parabéns, você acertou o resultado")
-elif numero_usuario > numero_da_sorte :
-    print("Que pena, você errou. Tente um número menor na próxima vez")
-else:
-    print("Que pena, você errou. Tente um número maior na próxima vez")
+        # Teste do resultado com o print, indicando a direção do erro
+        if numero_usuario == numero_da_sorte:
+            print("Parabéns, você acertou o resultado")
+        elif numero_usuario > numero_da_sorte :
+            print("Que pena, você errou. Tente um número menor na próxima vez")
+        else:
+            print("Que pena, você errou. Tente um número maior na próxima vez") 
+    
+        # Sai do programa após o teste realizado
+        break
+
+    except ValueError as err:
+        print("Insira um valor numérico inteiro para fazer o teste")
+
+# %%
